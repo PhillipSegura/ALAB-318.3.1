e60a84bad35387feb58de5690838f3df3b173ec3
PhillipSegura <PhillipaSegura@gmail.com>
2024-11-27 23:55:42 -0700

    completed majority as a class and added vegetable.js in group study session

---
+++ data/vegetables.js
@@ -0,0 +1,25 @@
+//  copied the structure from the fruits.js file and adapted it to vegetables
+const vegetables = [
+  {
+    name: "green-beans",
+    color: "green",
+    readyToEat: true,
+  },
+  {
+    name: "carrot",
+    color: "yellow",
+    readyToEat: false,
+  },
+  {
+    name: "broccoli",
+    color: "green",
+    readyToEat: true,
+  },
+  {
+    name: "bell-pepper",
+    color: "red",
+    readyToEat: true,
+  },
+];
+
+module.exports = vegetables;
